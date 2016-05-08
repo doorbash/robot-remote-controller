9f67295525286d839634217f58ce6080219734b3
Milad Doorbash <milad.doorbash@gmail.com>
2016-05-08 01:26:43 -0400

    Add socket-io

---
+++ client.py
@@ -0,0 +1,44 @@
+import serial
+import pygame
+import time
+import socket
+
+from socketIO_client import SocketIO, LoggingNamespace
+
+with SocketIO('127.0.0.1', 3000, LoggingNamespace) as socketIO:
+
+    pygame.init()
+    j = pygame.joystick.Joystick(0)
+    j.init()
+    print 'Initialized Joystick : %s' % j.get_name()
+
+    numButtons = j.get_numbuttons()
+    numAxes = j.get_numaxes()
+    numHats = j.get_numhats()
+
+    try:
+        while True:
+            pygame.event.pump()
+
+            data = 0
+
+            for i in range(0, numButtons):
+                if j.get_button(i) != 0:
+                    data += (1 << i)
+
+            for i in range(numButtons,numButtons + numAxes*2)[::2]:
+                axis = j.get_axis((i-numButtons)/2)
+                if axis > 0:
+                    data += 1 << i
+                elif axis < 0:
+                    data += 1 << i + 1
+
+            if data > 0:
+                socketIO.emit('event',str(data))
+
+            
+
+    except KeyboardInterrupt:
+        j.quit()
+    except Exception, e:
+        print e
---
+++ server.js
@@ -0,0 +1,15 @@
+var io = require('socket.io')();
+io.on('connection', function(socket)
+{
+	console.log('client connected')
+	socket.on('event', function(data)
+	{
+		bin = parseInt(data).toString(2)
+		bin = "0".repeat(30-bin.length) + bin
+		console.log(bin)
+	});
+	socket.on('disconnect', function() {
+		console.log('disconnected')
+	});
+});
+io.listen(3000);
